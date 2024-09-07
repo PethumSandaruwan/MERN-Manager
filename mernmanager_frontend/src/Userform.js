76e95753ae6ac2179d4ab37270db69c0d45718da
Pethum Sandaruwan <pethumsandaruwan007@gmail.com>
2024-09-07 23:45:01 +0530

    create user and get users

--- mernmanager_frontend/src/Userform.js
+++ mernmanager_frontend/src/Userform.js
@@ -1,92 +1,104 @@
 import { Grid, Input, Typography, Button } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const Userform = (props) => {
+const Userform = ({ addUser,submitted }) => {
 
-    const [id,setID]=useState(0);
-    const [name,setName]=useState("");
+  const [id, setID] = useState(0);
+  const [name, setName] = useState("");
 
-    return (
-        <Grid
-            container
-            spacing={2}
-            sx={{
-                backgroundColor: '#ffffff',
-                marginBottom: '30px',
-                display: 'block',
-            }}
+  useEffect(()=>{
+    if(!submitted){
+        setID(0);
+        setName("");
+    }
+  },[submitted])
+
+  const handleAddUser = () => {
+    addUser({ id, name });
+  };
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: '#ffffff',
+        marginBottom: '30px',
+        display: 'block',
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography component="h1" sx={{ color: '#000000' }}>
+          User Form
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sx={{ display: 'flex' }}>
+        <Typography
+          component="label"
+          htmlFor="id"
+          sx={{
+            color: '#000000',
+            marginRight: '20px',
+            fontSize: '16px',
+            width: '100px',
+            display: 'block',
+          }}
         >
-            <Grid item xs={12}>
-                <Typography component="h1" sx={{ color: '#000000' }}>
-                    User Form
-                </Typography>
-            </Grid>
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id="id"
+          name="id"
+          sx={{ width: '400px' }}
+          value={id}
+          onChange={e => setID(e.target.value)}
+        />
+      </Grid>
 
-            <Grid item xs={12} sx={{ display: 'flex' }}>
-                <Typography
-                    component="label"
-                    htmlFor="id"
-                    sx={{
-                        color: '#000000',
-                        marginRight: '20px',
-                        fontSize: '16px',
-                        width: '100px',
-                        display: 'block',
-                    }}
-                >
-                    ID
-                </Typography>
-                <Input
-                    type="number"
-                    id="id"
-                    name="id"
-                    sx={{ width: '400px' }}
-                    value={id}
-                    onChange={e => setID(e.target.value)}
-                />
-            </Grid>
+      <Grid item xs={12} sx={{ display: 'flex' }}>
+        <Typography
+          component="label"
+          htmlFor="name"
+          sx={{
+            color: '#000000',
+            marginRight: '20px',
+            fontSize: '16px',
+            width: '100px',
+            display: 'block',
+          }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          sx={{ width: '400px' }}
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </Grid>
 
-            <Grid item xs={12} sx={{ display: 'flex' }}>
-                <Typography
-                    component="label"
-                    htmlFor="name"
-                    sx={{
-                        color: '#000000',
-                        marginRight: '20px',
-                        fontSize: '16px',
-                        width: '100px',
-                        display: 'block',
-                    }}
-                >
-                    Name
-                </Typography>
-                <Input
-                    type="text"
-                    id="name"
-                    name="name"
-                    sx={{ width: '400px' }}
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-            </Grid>
-
-            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Button
-                    sx={{
-                        backgroundColor: "#00c6e6",
-                        color: '#000000',
-                        marginTop: "20px",
-                        "&:hover": {
-                            opacity: '0.7',
-                            backgroundColor: '#00c6e6',
-                        }
-                    }}
-                >
-                    Add
-                </Button>
-            </Grid>
-        </Grid>
-    );
-}
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Button
+          sx={{
+            backgroundColor: "#00c6e6",
+            color: '#000000',
+            marginTop: "20px",
+            "&:hover": {
+              opacity: '0.7',
+              backgroundColor: '#00c6e6',
+            }
+          }}
+          onClick={handleAddUser} // Use a function reference here
+        >
+          Add
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Userform;
--- mernmanager_frontend/src/Users.js
+++ mernmanager_frontend/src/Users.js
@@ -1,20 +1,45 @@
 import { Box } from "@mui/material";
 import Userform from "./Userform";
 import Usertable from "./Usertable";
+import  Axios  from "axios";
+import { useEffect, useState } from "react";
 
-const users=[
-  {  
-    id:1,
-    name:'pethum',
 
-},
-{
-    id:2,
-    name:"Tharushi",
-}
-];
 
 const Users=()=>{
+    const [users,setUsers]=useState([]);
+    const [submitted,setSubmitted]=useState(false);
+
+    useEffect(()=>{
+        getUsers();
+    },[]);
+
+    const getUsers=()=>{
+        Axios.get('http://localhost:3001/api/users')
+            .then(response=>{
+                setUsers(response?.data?.response||[])
+            })
+            .catch(error=>{
+                console.log("axios error:",error);
+            });
+    }
+
+    const addUser=(data)=>{
+        setSubmitted(true);
+        const payload={
+            id:data.id,
+            name:data.name,
+        }
+        Axios.post('http://localhost:3001/api/createuser',payload)
+        .then(()=>{
+            getUsers();
+            setSubmitted(false)
+            
+        })
+        .catch(error=>{
+            console.log("axios error:",error);
+        });
+    }
     return(
         <Box
             sx={{
@@ -24,7 +49,10 @@ const Users=()=>{
 
             }}
         >
-        <Userform/>
+        <Userform
+        addUser={addUser}
+        submitted={submitted}
+        />
         <Usertable rows={users}/>
         </Box>
     );
